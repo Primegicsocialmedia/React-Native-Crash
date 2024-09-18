40fbb4fb552555cb5257d495d3ef4370c05f4eb4
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-18 16:06:27 +0200

    onboarding page complete

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -12,7 +12,8 @@ const TabIcon = ({ icon, color, name, focused }) => {
         className="w-6 h-6 "
       />
       <Text
-        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`} style={{color: color}}
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
       >
         {name}
       </Text>
@@ -26,14 +27,14 @@ const TabsLayout = () => {
       <Tabs
         screenOptions={{
           tabBarShowLabel: false,
-          tabBarActiveTintColor: '#FFA001',
-          tabBarInactiveTintColor: '#CDCDE0',
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
           tabBarStyle: {
-            backgroundColor: '#151622',
+            backgroundColor: "#151622",
             borderTopWidth: 1,
-            borderTopColor: '#232533',
+            borderTopColor: "#232533",
             height: 84,
-          }
+          },
         }}
       >
         <Tabs.Screen
